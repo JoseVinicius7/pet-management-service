3e64069070d2a3fd42c12c3a4b53a81e7a0d33ce
José Vinicius <jose.vinicius7@hotmail.com>
2024-10-07 12:30:13 -0300

    first commit

---
+++ src/test/java/br/com/app/pet_management_service/PetManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.pet_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
